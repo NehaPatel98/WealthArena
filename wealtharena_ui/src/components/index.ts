02b20f151d6548015e009b10e5f3afde964b951f
NehaPatel98 <169084141+NehaPatel98@users.noreply.github.com>
2025-10-01 11:16:43 -0400

    Delete wealtharena_ui directory

--- wealtharena_ui/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { Header } from './ui/Header'
-export { Card } from './ui/Card'
-export { MetricPill } from './ui/MetricPill'
-export { InsightItem } from './ui/InsightItem'
-export { LineChartPlaceholder } from './ui/LineChartPlaceholder'
-
